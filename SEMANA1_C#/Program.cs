a64f912053f9c9115ac2696222c0e34a55e9463a
Eduardo <eduardopuescas5gmail.com>
2025-09-02 15:30:23 -0500

    subiendo ejercicios1-SEM1

---
+++ SEMANA1_C#/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEMANA1_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ejer1();
+            Console.ReadKey();
+        }
+
+        static void ejer1()
+        {
+            string nombre, carrera; //Declaramos variables
+
+            Console.Write("Ingrese su nombre: ");
+            nombre = Console.ReadLine();
+            Console.Write("Ingrese su nombre de carrera: ");
+            carrera = Console.ReadLine();
+
+            Console.WriteLine($"\n{nombre}, bienvenido a FA de {carrera}");
+
+
+        }
+        static void ejer2()
+        {
+
+        }
+         static void ejer3()
+        {
+
+        }
+        static void ejer4()
+        {
+
+        }
+        static void ejer5()
+        {
+
+        }
+        static void ejer6()
+        {
+
+        }
+    }
+}
---
+++ SEMANA1_PYTHON/SEMANA1_PYTHON.py
@@ -0,0 +1,8 @@
+def ejer1():
+    nombre = input("Ingrese su nombreP: ")
+    carrera = input("Ingrese su carrera: ")
+
+    print(f"\n{nombre}, bienvenido a FA de {carrera}")
+    
+ejer1()
+    
